11fc8327b60a673a9bbf08bcf5691b495571fac9
Etienne Naude <software.etinaude.dev>
2021-09-12 11:44:35 +0000

    updated link addresses

--- front/src/app/global.ts
+++ front/src/app/global.ts
@@ -1,2 +1,1 @@
-export var ip = "http://121.98.243.22:3000/tracker/api/v1/";
-// export var ip = "http://localhost:3000/tracker/api/v1/";
+export var ip = "https://back.etinaude.dev/tracker/api/v1/"
